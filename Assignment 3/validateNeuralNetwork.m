b6c062b41c71907a9dcbd3c5b0395aeabbbf0d0c
hermannsblum <hermannsblum@yahoo.de>
2015-11-19 11:22:19 +0000

    architecture changes: moved code repetitions in extra function for readability

--- Assignment 3/validateNeuralNetwork.m
+++ Assignment 3/validateNeuralNetwork.m
@@ -48,41 +48,9 @@ switch algorithm
                             disp(['Testing parameters npl=' num2str(npl)...
                                 ' l=' num2str(l) ' lr=' num2str(lr)...
                                 ' lr_dec=' num2str(lr_dec) ' lr_inc=' num2str(lr_inc)]);
-                            % From http://uk.mathworks.com/help/nnet/ug/improve-neural-network-generalization-and-avoid-overfitting.html?refresh=true
-                            % Train different networks and take the one
-                            % with the minimum mse to avoid overfitting
-                            NN = cell(5,1);
-                            perfs = zeros(5,1);
-                            for j = 1:5 
-                                % Set indices for training and validation
-                                % sets
-                                net.divideFcn = 'divideind';
-                                net.divideParam.trainInd = trainingSetIndices;
-                                net.divideParam.valInd = validationSetIndices;
-                                net.divideParam.testInd = [];
-
-                                % TODO: Set overfitting avoidance
-                                % parameters
-
-                                % Set up input and output layer
-                                NN{j} = configure(net, attributesNN, labelsNN);
-                                % Train network
-                                [NN{j}, trainRecord] = train(net, attributesNN, labelsNN);
-                                % Evaluate mean squared error on validation
-                                % set. trainRecord.best_vperf contains the
-                                % optimal error found in validation set
-                                % during the training
-                                perfs(j) = trainRecord.best_vperf;
-                            end
-                            [~, jOpt] = min(perfs(j));
-                            net = NN{jOpt};
-                            % Get performance on validation set
-                            predictions = NNout2labels(sim(net, attributesNN(:,validationSetIndices)));
-                            confMatrix = getConfusionMatrix(labels(validationSetIndices),predictions,6);
-                            
-                            % Compute average accuracy for the current
-                            % parameter configuration
-                            accuracies(a,b,c,d,e) = sum(diag(confMatrix))/length(predictions);
+                            accuracies(a,b,c,d,e) = findMinimumError(net, ...
+                                attributesNN, labelsNN, trainingSetIndices, ...
+                                validationSetIndices);
                             
                         end
                     end
@@ -118,39 +86,58 @@ switch algorithm
                         delt_dec = delta_dec(d);
                         % Set delta dec
                         net.trainParam.delt_dec = delt_dec;
-
-                        trialsAccuracies = zeros(1,5);
-                        for j = 1:5
-                            % Set indices for training and validation
-                            % sets
-                            net.divideFcn = 'divideind';
-                            net.divideParam.trainInd = trainingSetIndices;
-                            net.divideParam.valInd = validationSetIndices;
-                            net.divideParam.testInd = [];
-
-
-                            % TODO: Set overfitting avoidance
-                            % parameters
-
-                            % Set up input and output layer
-                            net = configure(net, attributesNN, labelsNN);
-                            % Train network
-                            net = train(net, attributesNN, labelsNN);
-                            % Get performance on validation set
-                            predictions = NNout2labels(sim(net, attributesNN(:,validationSetIndices)));
-                            confMatrix = getConfusionMatrix(labels(validationSetIndices),predictions,6);
-                            trialsAccuracies(j) = sum(diag(confMatrix))/length(predictions);
-                            
-                        end
-                        % Compute average accuracy for the current
-                        % parameter configuration
-                        accuracies(a,b,c,d) = mean(trialsAccuracies);
-
                         
+                        accuracies(a, b, c, d) = findMinimumError(net, ...
+                            attributesNN, labelsNN, trainingSetIndices, ...
+                            validationSetIndices);
                     end
                 end
             end
         end
 
 end
+end
 
+
+function [accuracy] = findMinimumError(net, attributesNN, labelsNN, ...
+    trainingSetIndices, validationSetIndices)
+% train 5 networks with random start weights and  choose the one with the 
+% minumum Error
+
+% From http://uk.mathworks.com/help/nnet/ug/improve-neural-network-generalization-and-avoid-overfitting.html?refresh=true
+% Train different networks and take the one
+% with the minimum mse to avoid overfitting
+
+labels = NNout2labels(labelsNN);
+
+NN = cell(5,1);
+perfs = zeros(5,1);
+for j = 1:5
+    % Set indices for training and validation
+    % sets
+    net.divideFcn = 'divideind';
+    net.divideParam.trainInd = trainingSetIndices;
+    net.divideParam.valInd = validationSetIndices;
+    net.divideParam.testInd = [];
+
+    % Set up input and output layer
+    NN{j} = configure(net, attributesNN, labelsNN);
+    % Train network
+    [NN{j}, trainRecord] = train(NN{j}, attributesNN, labelsNN);
+    % Evaluate mean squared error on validation
+    % set. trainRecord.best_vperf contains the
+    % optimal error found in validation set
+    % during the training
+    perfs(j) = trainRecord.best_vperf;
+end
+[~, jOpt] = min(perfs(j));
+net = NN{jOpt};
+
+% Get performance on validation set
+predictions = NNout2labels(sim(net, attributesNN(:,validationSetIndices)));
+confMatrix = getConfusionMatrix(labels(validationSetIndices),predictions,6);
+
+% Compute average accuracy for the current
+% parameter configuration
+accuracy = sum(diag(confMatrix))/length(predictions);
+end
